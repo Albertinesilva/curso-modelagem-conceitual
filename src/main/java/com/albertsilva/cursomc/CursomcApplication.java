3e38cf63317daf655dc1664db51a349c30a9e8b7
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2026-02-04 19:34:00 -0300

    Estado e Cidade

--- src/main/java/com/albertsilva/cursomc/CursomcApplication.java
+++ src/main/java/com/albertsilva/cursomc/CursomcApplication.java
@@ -7,8 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.albertsilva.cursomc.domain.Categoria;
+import com.albertsilva.cursomc.domain.Cidade;
+import com.albertsilva.cursomc.domain.Estado;
 import com.albertsilva.cursomc.domain.Produto;
 import com.albertsilva.cursomc.repositories.CategoriaRepository;
+import com.albertsilva.cursomc.repositories.CidadeRepository;
+import com.albertsilva.cursomc.repositories.EstadoRepository;
 import com.albertsilva.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -16,10 +20,17 @@ public class CursomcApplication implements CommandLineRunner {
 
 	private final CategoriaRepository categoriaRepository;
 	private final ProdutoRepository produtoRepository;
+	private final EstadoRepository estadoRepository;
+	private final CidadeRepository cidadeRepository;
 
-	public CursomcApplication(CategoriaRepository categoriaRepository, ProdutoRepository produtoRepository) {
+	public CursomcApplication(CategoriaRepository categoriaRepository,
+			ProdutoRepository produtoRepository,
+			EstadoRepository estadoRepository,
+			CidadeRepository cidadeRepository) {
 		this.categoriaRepository = categoriaRepository;
 		this.produtoRepository = produtoRepository;
+		this.estadoRepository = estadoRepository;
+		this.cidadeRepository = cidadeRepository;
 	}
 
 	public static void main(String[] args) {
@@ -30,6 +41,7 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		/* Categorias e Produtos */
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 
@@ -46,6 +58,20 @@ public class CursomcApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		/* Estados e Cidades */
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 	}
 
--- src/main/java/com/albertsilva/cursomc/domain/Cidade.java
+++ src/main/java/com/albertsilva/cursomc/domain/Cidade.java
@@ -1,41 +1,34 @@
 package com.albertsilva.cursomc.domain;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 
-@Entity(name = "produto")
-public class Produto implements Serializable {
+@Entity(name = "cidade")
+public class Cidade implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   private String nome;
-  private Double preco;
 
-  @JsonBackReference
-  @ManyToMany
-  @JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
-  private List<Categoria> categorias = new ArrayList<>();
+  @ManyToOne
+  @JoinColumn(name = "estado_id")
+  private Estado estado;
 
-  public Produto() {
+  public Cidade() {
   }
 
-  public Produto(Integer id, String nome, Double preco) {
+  public Cidade(Integer id, String nome, Estado estado) {
     this.id = id;
     this.nome = nome;
-    this.preco = preco;
+    this.estado = estado;
   }
 
   public Integer getId() {
@@ -54,20 +47,12 @@ public class Produto implements Serializable {
     this.nome = nome;
   }
 
-  public Double getPreco() {
-    return preco;
+  public Estado getEstado() {
+    return estado;
   }
 
-  public void setPreco(Double preco) {
-    this.preco = preco;
-  }
-
-  public List<Categoria> getCategorias() {
-    return categorias;
-  }
-
-  public void setCategorias(List<Categoria> categorias) {
-    this.categorias = categorias;
+  public void setEstado(Estado estado) {
+    this.estado = estado;
   }
 
   @Override
@@ -86,7 +71,7 @@ public class Produto implements Serializable {
       return false;
     if (getClass() != obj.getClass())
       return false;
-    Produto other = (Produto) obj;
+    Cidade other = (Cidade) obj;
     if (id == null) {
       if (other.id != null)
         return false;
