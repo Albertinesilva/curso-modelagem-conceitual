dd865a6455eae96a1a0655c22a0077345eebf7d9
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2026-02-03 22:26:16 -0300

    Banc de dados H2 e criação automatica da base de dados

--- src/main/java/com/albertsilva/cursomc/domain/Categoria.java
+++ src/main/java/com/albertsilva/cursomc/domain/Categoria.java
@@ -2,10 +2,17 @@ package com.albertsilva.cursomc.domain;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity(name = "categoria")
 public class Categoria implements Serializable {
   private static final long serialVersionUID = 1L;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   private String nome;
 
